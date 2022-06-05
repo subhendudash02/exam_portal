f5410586bdd9ba539eecaa1bc868a93b79a6e788
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-06-05 13:47:47 +0530

    remove unecessary imports

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,11 +1,9 @@
-from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
 from .models import User
 import jwt
-import datetime
 
 # Create your views here.
 class Register(APIView):
