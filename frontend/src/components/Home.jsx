1dd4dd717c9a8bc88ae2ca194eeb31c5c32a21a0
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-06-08 23:45:42 +0530

    add css for homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import '../styles/App.css';
 
 export default function Home() {
     return (
         <div className="home">
-            <h1>Welcome to the portal</h1>
-            <Link to="/signin">Sign In</Link><br />
-            <Link to="/signup">Sign Up</Link>
+            <h1 className='heading'>Welcome to the portal</h1>
+            <p className="para">One place for all exams!</p>
+            <button className="signin"><Link to="/signin">Sign In</Link></button>
         </div>
     );
 }
