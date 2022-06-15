263fcc52fce82ca7611373fd11c39140671c48e3
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-06-15 16:41:20 +0530

    modify api

--- frontend/src/utils/question.js
+++ frontend/src/utils/question.js
@@ -1,18 +1,30 @@
 // api call later
 
 const list = [
-    {
-        id: 1,
-        desc: "one",
-    },
-    {
-        id: 2,
-        desc: "two",
-    },
-    {
-        id: 3,
-        desc: "three",
-    },
+    {   
+        test_id: 1,
+        test_name: "demo",
+        start: "Wed Jun 15 2022 15:00:00",
+        end: "Wed Jun 15 2022 17:00:00",
+        questions: [
+            {
+                ques_id: 1,
+                rows: [
+                {
+                    id: 1,
+                    desc: "one",
+                },
+                {
+                    id: 2,
+                    desc: "two",
+                },
+                {
+                    id: 3,
+                    desc: "three",
+                },]
+            }
+        ]
+    }
 ];
 
 export default list;
